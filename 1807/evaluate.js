e9209a0a5e70350253608d7ba971c2a934232b96
Douglas Chen <cdswyda@hotmail.com>
2023-01-12 23:28:35 +0800

    1807

---
+++ 1807/evaluate.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @param {string[][]} knowledge
+ * @return {string}
+ */
+var evaluate = function (s, knowledge) {
+  var map = new Map();
+  var i = 0, len = knowledge.length;
+  for (; i < len; i++) {
+    map.set(knowledge[i][0], knowledge[i][1]);
+  }
+
+  i = 0; len = s.length;
+
+  var arr = [];
+  var key = '';
+  var isGroup = false;
+
+  for (; i < len; i++) {
+    if (s[i] === '(') {
+      isGroup = true;
+    } else if (s[i] === ')') {
+      // console.log(key);
+      if (map.has(key)) {
+        arr.push(map.get(key));
+      } else {
+        arr.push('?');
+      }
+      key = '';
+      isGroup = false;
+    } else {
+      if (isGroup) {
+        key += s[i];
+      } else {
+        arr.push(s[i]);
+      }
+    }
+
+  }
+  return arr.join('');
+};
+
+// console.log(evaluate("(name)is(age)yearsold", [["name", "bob"], ["age", "two"]]));
+// console.log(evaluate("hi(name)", [["a", "b"]]));
+// console.log(evaluate("(a)(a)(a)aaa", [["a", "yes"]]));
