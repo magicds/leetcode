6b440bdd468504c78c275d7df49a5a0568ff98e0
chends <cdswyda@hotmail.com>
2023-08-22 19:53:29 +0800

    add 849

---
+++ 849-maximize-distance-to-closest-person/go/main.go
@@ -0,0 +1,37 @@
+func maxDistToClosest(seats []int) int {
+	n, i, j, dist := len(seats), 0, 0, 0;
+	for i < n {
+		if seats[i] == 1 {
+			break
+		}
+		i++
+	}
+	dist = i // 左端的最大距离
+	for i < n {
+		j = i + 1
+		// 向右找到下一个人
+		for j < n && seats[j] == 0 {
+			j++
+		}
+		if j == i + 1 {
+			i = j
+			continue
+		}
+		if j < n {
+			// 两人之间
+			dist = max(dist, (j - i) / 2)
+		} else {
+			// 最右端
+			dist = max(dist,  j - i - 1)
+		}
+		i = j
+	}
+	return dist
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ 849-maximize-distance-to-closest-person/js/index.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} seats
+ * @return {number}
+ */
+var maxDistToClosest = function (seats) {
+    const n = seats.length;
+    let i = 0,
+        j = 0;
+    let max = 0;
+    while (i < n) {
+        // i 左边第一个人
+        if (seats[i] === 1) {
+            break;
+        }
+        i++;
+    }
+    max = i; // 左边的有空位的话的距离
+
+    while (i < n) {
+        j = i + 1;
+        // 向右找到下一个人
+        while (j < n && seats[j] === 0) {
+            j++;
+        }
+        if (j === i + 1) {
+            i = j;
+            continue;
+        }
+        // console.log('between', i, j);
+        if (j < n) {
+            // 两人之间
+            max = Math.max(max, Math.floor((j - i) / 2));
+        } else {
+            // 最后一段
+            max = Math.max(max, j - i - 1);
+        }
+        i = j;
+    }
+    return max;
+};
+
+console.log(maxDistToClosest([1, 0, 0, 0, 1, 0, 1]));
+console.log(maxDistToClosest([1, 0, 0, 0]));
+console.log(maxDistToClosest([0, 1]));
