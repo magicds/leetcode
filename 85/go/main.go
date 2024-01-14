65724850ddf95a43f6497215642f57081c6e7a2b
magicds <cdswyda@hotmail.com>
2024-01-14 14:16:48 +0800

    85 [2024-01-14]

---
+++ 85/go/main.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// start
+func deleteDuplicates(head *ListNode) *ListNode {
+	node := head
+	for node != nil && node.Next != nil {
+		if node.Val == node.Next.Val {
+			node.Next = node.Next.Next
+		} else {
+			node = node.Next
+		}
+	}
+	return head
+}
---
+++ 85/ts/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let node = head;
+    while (node && node.next) {
+        if (node.val === node.next.val) {
+            node.next = node.next.next;
+        } else {
+            node = node.next;
+        }
+    }
+    return head;
+}
